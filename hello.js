19304cfac867dd6188d1ff974d5be4e410b83980
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2022-07-24 21:32:44 +0530

    adding hello and variables JS files

---
+++ hello.js
@@ -0,0 +1,7 @@
+console.log("Hello there, I am Akash Singh")
+console.log("Hello World")
+
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,44 @@
+         // Var
+
+var firstName="Akash";
+var firstName="Abhi";
+console.log(firstName);
+
+//      OR
+
+// var firstName="Akash";
+// firstName="Abhi";
+// console.log(firstName);
+
+
+        // let
+
+// let firstName="Akash";
+// firstName="Abhi";
+// console.log(firstName);
+
+
+//     OR 
+
+    //   Gives Error because let can't be re-declared
+
+// let firstName="Akash";
+// let firstName="Abhi";
+// console.log(firstName);
+
+
+
+// const
+
+
+const number=40;
+console.log(number);
+
+
+
+
+
+
+
+
+
