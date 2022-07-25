6cd762bdcfdc5449ae8e494eee648629df364a46
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2022-07-25 15:34:44 +0530

    adding updated variables and hello JS files

---
+++ variables.js
@@ -0,0 +1,43 @@
+         // Var
+
+var firstName="Akash";
+var firstName="Abhi";
+console.log(firstName);
+
+//      OR
+
+var firstName="Akash";
+firstName="Abhi";
+console.log(firstName);
+
+
+        // let
+
+let firstName="Akash";
+firstName="Abhi";
+console.log(firstName);
+
+
+//     OR 
+
+    //   Gives Error because let can't be re-declared
+
+let firstName="Akash";
+let firstName="Abhi";
+console.log(firstName);
+
+
+
+// const
+
+// const variable Should not be empty
+
+const number=40;
+console.log(number);
+
+
+
+
+
+
+
