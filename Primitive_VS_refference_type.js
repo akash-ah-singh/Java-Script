39e05de397655c237a3896db35ba8543e46ffed4
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2022-08-04 22:43:30 +0530

    Primitive VS reference type

---
+++ Primitive_VS_refference_type.js
@@ -0,0 +1,24 @@
+// Primitive type
+
+let num1=6;
+let num2=num1;
+console.log(num1);
+console.log(num2);
+num1++;
+console.log(num1);
+console.log(num2);
+
+
+// Reference type
+
+let arr1=[1,2,3,4,5];
+let arr2=arr1;
+console.log(arr1);
+console.log(arr2);
+arr1.push(10);
+console.log(arr1);
+console.log(arr2);
+
+
+
+
