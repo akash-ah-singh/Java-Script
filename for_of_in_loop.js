7d30f849c847ca35a4855cb5167d01136b5179ca
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2022-08-06 17:48:45 +0530

    for of in loop

---
+++ for_of_in_loop.js
@@ -0,0 +1,27 @@
+// for of loop
+
+const fruits1=["Akash","Abhi","Sid","Rahul"];
+
+for(let fruit of fruits1){
+    console.log(fruit);
+}
+
+
+// for in loop
+
+
+
+
+const fruits2=["Akash","Abhi","Sid","Rahul"];
+
+for(let index in fruits2){
+    console.log(index);
+}
+
+
+
+const fruits3=["Akash","Abhi","Sid","Rahul"];
+
+for(let index in fruits3){
+    console.log(fruits3[index]);
+}
