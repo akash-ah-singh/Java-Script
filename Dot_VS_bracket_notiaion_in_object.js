7bf3e884e173a346a0123d5068908e45b598af15
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2022-10-12 12:05:17 +0530

    Dot VS Bracket Notation In Object

---
+++ Dot_VS_bracket_notiaion_in_object.js
@@ -0,0 +1,28 @@
+
+
+const person={
+    name: "Akash",
+    age: 23,
+    "my hobbie": ["abc","xyz","pqr"]
+}
+
+// for extraction 
+
+// use square brackets instead of dot , to extract values from "2 word" keys
+
+console.log(person["my hobbie"])
+
+
+// for adding 
+
+// const key ="email";
+// const person={
+//     name: "Akash",
+//     age: 23,
+//     "my hobbie": ["abc","xyz","pqr"]
+// }
+
+
+// person[key]="abc@gmal.com";
+
+// console.log(person)
