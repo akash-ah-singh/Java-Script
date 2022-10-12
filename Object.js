de1c5a0354d10db27a3586a44e0afec28c2747ff
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2022-10-12 12:04:17 +0530

    Object

---
+++ Object.js
@@ -0,0 +1,33 @@
+// How to create Object
+
+// const person={name: "Akash", age: 23, hobbie: ["abc","xyz","pqr"]}
+
+// OR 
+
+const person={
+    name: "Akash",
+    age: 23,
+    hobbie: ["abc","xyz","pqr"]
+}
+
+
+// How to access an object 
+
+
+console.log(person);  // printing hole object person
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.hobbie);
+
+
+
+// how to add keys and values in an object 
+
+
+person.gender="male";
+console.log(person)
+
+
+
+// NOTE :-  By default keys are also string 
